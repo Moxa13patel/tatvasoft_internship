a86066d6ab2949fcddf6a7643f35853492e115af
Moxa13patel <117264446+Moxa13patel@users.noreply.github.com>
2025-07-04 15:19:25 +0530

    angular student-form

---
+++ day-2/student-form/src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import { MyFormComponent } from './STUDENT_FORM.component'; // adjust path
+
+export const routes: Routes = [
+  { path: '', component: MyFormComponent },
+];
+
